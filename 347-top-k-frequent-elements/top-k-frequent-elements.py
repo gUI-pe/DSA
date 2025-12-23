1c8aa8cbca3145f552dabe2ae8d22466dc7f13f4
Guilherme Peruci <121200588+gUI-pe@users.noreply.github.com>
2025-12-23 20:00:21 -0300

    Time: 7 ms (51.97%) | Memory: 20.8 MB (95.41%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:  
+        book = defaultdict(int)
+        # creates a dict to keep how many times an int is called
+        for i in nums:
+            book[i] += 1
+        # sorts the dict by value in descending order
+        book = dict(sorted(book.items(), key=lambda item: item[1]))
+
+        bookAnswers = list(book.keys())
+
+        answer = []
+        for _ in range (k):
+            answer.append(bookAnswers.pop())
+
+        return answer
